c108628c8b0c42df7f803f8fcdf5c194eabda7d8
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-15 11:45:37 +0530

    2. Operators.py
    Operators are symbols used to perform operation on values.

---
+++ Day 2 - INPUT + OPERATORS.py/2. Operators.py
@@ -0,0 +1,43 @@
+Def: Operators are symbols used to perform operation on values.
+
+Program: Arthimetic operators
+a = 6
+b = 3
+c = 7
+print("Addition(+): The + operator is used to add 2 values.")
+print("a + b =", a + b)
+print("Subtraction(-): The - operator is used to subtract from one value from another value.")
+print("a - b =", a - b)
+print("Multiplication(*): The * operator is used to multiply two values.")
+print("a * b =", a * b)
+print("Division(/): The / operator is used to divide one value by anothe value and returns float results.")
+print("Real Ex: 10 chocolates 2 people ki share chesthe okkokariki 5")
+print("c / b =", c / b)
+print("Modulus(%): The % operator returns the remainder after division.")
+print("Real Ex: 10 chocolates 3 people ki ivvali then 3 each → 1 migilindhi")
+print("c % a =", c % a)
+print("Floor Division(//): The // operator divides and returns only the integer part (no decimals).")
+print("Real Ex: 10 / 3 = 3.33 then → 3 matrame tiskuntundi because decimal remove chesi integer matrame tiskuntundi.")
+print("c // a =", c // a)
+print("Exponent(**): The ** operator is used to raise a number to the power of another.")
+print("Real Ex: power calculation 2 power 3 = 2 × 2 × 2 = 8")
+
+--Output:
+Addition(+): The + operator is used to add 2 values.
+a + b = 9
+Subtraction(-): The - operator is used to subtract from one value from another value.
+a - b = 3
+Multiplication(*): The * operator is used to multiply two values.
+a * b = 18
+Division(/): The / operator is used to divide one value by anothe value and returns float results.
+Real Ex: 10 chocolates 2 people ki share chesthe okkokariki 5
+c / b = 2.3333333333333335
+Modulus(%): The % operator returns the remainder after division.
+Real Ex: 10 chocolates 3 people ki ivvali then 3 each → 1 migilindhi
+c % a = 1
+Floor Division(//): The // operator divides and returns only the integer part (no decimals).
+Real Ex: 10 / 3 = 3.33 then → 3 matrame tiskuntundi because decimal remove chesi integer matrame tiskuntundi.
+c // a = 1
+Exponent(**): The ** operator is used to raise a number to the power of another.
+Real Ex: power calculation 2 power 3 = 2 × 2 × 2 = 8
+ a ** b = 216
