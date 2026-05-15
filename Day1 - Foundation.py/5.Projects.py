63762a8a047d657d6ac85ddbcb0d199621a67d0d
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-15 10:33:17 +0530

    5.Projects.py
    This project contain print(), variable, Data Types and Comments.

---
+++ Day1 - Foundation.py/5.Projects.py
@@ -0,0 +1,33 @@
+--Program: Display person details
+--------------------------------------
+
+name = "Raji"
+age = 25
+is_working = True
+profession = "Software"
+salary = 8.5
+location = "Hyderabad"
+print("See My details here")
+print("        |          ")
+print("       < >         ")
+print("My Name is :", name)
+print("My age is :", age)
+print("I am working :", is_working)
+print("My profession is :", profession)
+print("My salary is :", salary)
+print("My location is :", location)
+print("                                  ")
+print("..........@THANKS@..........")
+
+---Output:
+See My details here
+        |          
+       < >         
+My Name is : Raji
+My age is : 25
+I am working : True
+My profession is : Software
+My salary is : 8.5
+My location is : Hyderabad
+                                  
+..........@THANKS@..........
