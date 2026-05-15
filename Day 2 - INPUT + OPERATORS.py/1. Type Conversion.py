506c8809f87d08f9eafed5fefc30071763765e03
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-15 11:22:38 +0530

    1. Type Conversion.py
    Type Conversion is used to converting one data type into another data type.

---
+++ Day 2 - INPUT + OPERATORS.py/1. Type Conversion.py
@@ -0,0 +1,26 @@
+-- Def: Type Conversion is used to converting one data type into another data type.
+
+Program 1:
+----------
+age = int(input("Enter your age: "))
+print("Here Enter your age is string and 25 is integer")
+print("So we need to convert from string to integer for that we need to use int(input())")
+
+--Output:
+Enter your age:  25
+Here Enter your age is string and 25 is integer
+So we need to convert from string to integer for that we need to use int(input())
+
+Program 2:
+-----------
+salary1 = int(input("Enter your 1st salary: "))
+salary2 = int(input("Enter your 2nd salary: "))
+num = salary1 + salary2
+print(num) 
+
+--Output:
+Enter your 1st salary:  440000
+Enter your 2nd salary:  800000
+1240000
+                                            
+                                          
