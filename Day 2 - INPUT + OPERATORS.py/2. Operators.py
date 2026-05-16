4bcc21705e9789b2910f1a489728ebc6d6ff8d87
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-16 16:00:22 +0530

    2. Operators.py
    Operators are symbols used to perform operation on values.

--- Day 2 - INPUT + OPERATORS.py/2. Operators.py
+++ Day 2 - INPUT + OPERATORS.py/2. Operators.py
@@ -11,13 +11,13 @@ print("a - b =", a - b)
 print("Multiplication(*): The * operator is used to multiply two values.")
 print("a * b =", a * b)
 print("Division(/): The / operator is used to divide one value by anothe value and returns float results.")
-print("Real Ex: 10 chocolates 2 people ki share chesthe okkokariki 5")
+print("Real Ex: If 10 chocolates are shared among 2 people, each person gets 5.")
 print("c / b =", c / b)
 print("Modulus(%): The % operator returns the remainder after division.")
-print("Real Ex: 10 chocolates 3 people ki ivvali then 3 each → 1 migilindhi")
+print("Real Ex: 10 chocolates should be distributed among 3 people.If each person gets 3 chocolates ->1 remains")
 print("c % a =", c % a)
 print("Floor Division(//): The // operator divides and returns only the integer part (no decimals).")
-print("Real Ex: 10 / 3 = 3.33 then → 3 matrame tiskuntundi because decimal remove chesi integer matrame tiskuntundi.")
+print("Real Ex: 10 / 3 = 3.33 then → It takes only 3 because the decimal part is removed and only the integer value is considered.")
 print("c // a =", c // a)
 print("Exponent(**): The ** operator is used to raise a number to the power of another.")
 print("Real Ex: power calculation 2 power 3 = 2 × 2 × 2 = 8")
@@ -29,14 +29,14 @@ Subtraction(-): The - operator is used to subtract from one value from another v
 a - b = 3
 Multiplication(*): The * operator is used to multiply two values.
 a * b = 18
-Division(/): The / operator is used to divide one value by anothe value and returns float results.
-Real Ex: 10 chocolates 2 people ki share chesthe okkokariki 5
+Division(/): The / operator is used to divide one value by another value and returns float results.
+Real Ex: If 10 chocolates are shared between 2 people, each person gets 5 chocolates.
 c / b = 2.3333333333333335
 Modulus(%): The % operator returns the remainder after division.
-Real Ex: 10 chocolates 3 people ki ivvali then 3 each → 1 migilindhi
+Real Ex: If 10 chocolates are distributed among 3 people, each person gets 3 chocolates and 1 chocolate remains.
 c % a = 1
 Floor Division(//): The // operator divides and returns only the integer part (no decimals).
-Real Ex: 10 / 3 = 3.33 then → 3 matrame tiskuntundi because decimal remove chesi integer matrame tiskuntundi.
+Real Ex: 10 / 3 = 3.33 then → It takes only 3 because the decimal part is removed and only the integer value is considered.
 c // a = 1
 Exponent(**): The ** operator is used to raise a number to the power of another.
 Real Ex: power calculation 2 power 3 = 2 × 2 × 2 = 8
