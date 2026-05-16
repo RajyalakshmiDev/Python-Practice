0a5b152d79e9f86f89e50ea2ac245477affe2544
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-16 16:59:47 +0530

    4.Project.py
    Simple Calculator

---
+++ Day 2 - INPUT + OPERATORS.py/4.Project.py
@@ -0,0 +1,22 @@
+---Project: Simple Calculator
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Modulus:", num1 % num2)
+print("Floor Division:", num1 // num2)
+print("Exponent:", num1 ** num2)
+
+---OutPut
+Enter first number:  6
+Enter second number:  9
+Addition: 15
+Subtraction: -3
+Multiplication: 54
+Division: 0.6666666666666666
+Modulus: 6
+Floor Division: 0
+Exponent: 10077696
