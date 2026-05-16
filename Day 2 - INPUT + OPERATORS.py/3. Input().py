61c76e88b90ba4e8b7fdf22eb166bb164644ca73
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-16 16:30:15 +0530

    3. Input().py
    input() is used to take input(data) from the user.

--- Day 2 - INPUT + OPERATORS.py/3. Input().py
+++ Day 2 - INPUT + OPERATORS.py/3. Input().py
@@ -1,3 +1,10 @@
 Def: input() is used to take input(data) from the user.
 
-Program: 
+Program: Print name 
+name = input("Enter your name: ")
+print(name)
+
+--Output
+Enter your name:  Rajyalakshmi
+Rajyalakshmi
+
