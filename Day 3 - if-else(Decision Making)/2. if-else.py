82ff856b0a9743292310ca6159c4ea42a1a4d465
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-17 10:36:51 +0530

    2. if-else.py
    if-else is used to execute one block of code if condition is True, and another block if it is false.

---
+++ Day 3 - if-else(Decision Making)/2. if-else.py
@@ -0,0 +1,11 @@
+--- Def: if-else is used to execute one block of code if condition is True, and another block if it is false.
+
+Program: Check age conditions
+age = 16
+if age >= 17:
+  print("You are not Eligible")
+else:
+  print("You are Eligible")
+
+--- Output:
+You are Eligible
