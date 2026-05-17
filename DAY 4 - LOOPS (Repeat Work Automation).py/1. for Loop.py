004420c904a1856f920047edf60c44c6225d531d
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-17 19:34:35 +0530

    1. for Loop.py
    for = fixed repeat

---
+++ DAY 4 - LOOPS (Repeat Work Automation).py/1. for Loop.py
@@ -0,0 +1,12 @@
+--- Def: A for loop is used to repeat a block of code a fixed number of times.
+
+Program: Print 5 times Hello
+for i in range(5):
+  print("Hello")
+
+--- Output:
+Hello
+Hello
+Hello
+Hello
+Hello
