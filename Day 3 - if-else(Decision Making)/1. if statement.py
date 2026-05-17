5ae653e062799ba5d2b5c072d5e9e7af012f3fe3
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-17 08:31:19 +0530

    1. if statement.py
    The if statement is used to check a condition and execute code only if the condition is True.

---
+++ Day 3 - if-else(Decision Making)/1. if statement.py
@@ -0,0 +1,10 @@
+--- Def: The if statement is used to check a condition and execute code only if the condition is True.
+
+Program: Check age Condition.
+------------------------------
+age = 25
+if age>20:
+    print("You are Eligible")
+
+--Output:
+You are Eligible
