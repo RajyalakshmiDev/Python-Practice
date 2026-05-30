82927b00a887fc9c80ea779b54d8f049e2c40c6b
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-31 04:27:04 +0530

    5. continue.py
    continue = SKIP

---
+++ Day 4 - LOOPS (Repeat Work Automation).py/5. continue.py
@@ -0,0 +1,15 @@
+--- Def: Continue skips the current iteration and moves to the next iteration.
+
+--- Example:
+for i in range(6):
+    if i == 3:
+        continue
+    print(i)
+
+---Output:
+0
+1
+2
+4
+5
+
