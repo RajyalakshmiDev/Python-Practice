9fa820d0f5408a43679bf0455c57d8b042db63ae
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-30 16:47:48 +0530

    2. range().py
    range() = Number supplier

---
+++ Day 4 - LOOPS (Repeat Work Automation).py/2. range().py
@@ -0,0 +1,15 @@
+-- Def: range() is used to generate a sequence of numbers.
+
+-- Types of range():
+1. range(stop)
+EX: range(5)
+-> starts from 0 and stops before 5
+O/P: 0, 1, 2, 3, 4
+
+2.range(start, stop)
+Ex: range(1, 6)
+o/p: 1, 2, 3, 4, 5
+
+3.range(start, stop, step)
+Ex: range(1, 10, 2)
+o/p: 1, 3, 5, 7, 9
