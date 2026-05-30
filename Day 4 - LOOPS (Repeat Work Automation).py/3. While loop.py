681a915f98d8400682eec3098152cb9667f824c3
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-30 17:00:15 +0530

    3. While loop.py
    While is used to Condition repeat.

---
+++ Day 4 - LOOPS (Repeat Work Automation).py/3. While loop.py
@@ -0,0 +1,18 @@
+--- Def: A while loop repeats code as long as the condition is true.
+
+-- Syntax:
+while condition:
+  code
+
+-- Example:
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+--Output:
+1
+2
+3
+4
+5
