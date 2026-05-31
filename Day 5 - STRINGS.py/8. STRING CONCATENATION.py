d32f76106edc628f8d45dd6fbf8ce02b840324f6
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-31 23:22:14 +0530

    STRING CONCATENATION.py
    It means + joins strings

---
+++ Day 5 - STRINGS.py/8. STRING CONCATENATION.py
@@ -0,0 +1,9 @@
+--- Def: Concatenation means joining two or more strings.
+
+Program: Join two strings
+first_name = "Raji"
+last_name ="kaipu"
+print(first_name + " " + last_name)
+
+--Output
+Raji kaipu
