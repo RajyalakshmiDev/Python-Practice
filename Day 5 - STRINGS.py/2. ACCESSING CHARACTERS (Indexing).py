21fd452ab56ecbca0fac33e91fa9cf165141d376
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-31 22:48:16 +0530

    2. ACCESSING CHARACTERS (Indexing).py
    Index starts from 0

---
+++ Day 5 - STRINGS.py/2. ACCESSING CHARACTERS (Indexing).py
@@ -0,0 +1,15 @@
+--- Def: Indexing is used to access individual characters in a string.
+->Index starts from 0
+
+Program: 
+name = "RAJI"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+
+--- Output:
+R
+A
+J
+I
