2c1fd73e0a3a806c7141d1d8a6a645201a7e33f1
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-31 22:10:22 +0530

    1. STRING.py
    String = Text Data

---
+++ Day 5 - STRINGS.py/1. STRING.py
@@ -0,0 +1,8 @@
+--- Def: A string is a sequence of characters enclosed within single quotes(' ') or double quotes(" ").
+
+--Program: Display string
+name = "Rajyalakshmi"
+print(name)
+
+--Output:
+Rajyalakshmi
