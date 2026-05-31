2f24b1cdc296534fa45a42e9cc4f1a298211cb4f
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-31 23:31:51 +0530

    9. Project.py
    Personal profile generator

---
+++ Day 5 - STRINGS.py/9. Project.py
@@ -0,0 +1,26 @@
+--- Project: Personal profile generator
+name = input("Enter your name: ")
+city = input("Enter your city: ")
+print("name: ", name.upper())
+print("city: ", city.upper())
+print("Total number of length: ", len(name))
+
+--Output:
+Enter your name:  Raji
+Enter your city:  Ongole
+name:  RAJI
+city:  ONGOLE
+Total number of length:  4
+
+
+----------------------------------
+---- FINAL MEMORY SHEET
+Method	      Purpose
+len()	        Count characters
+upper()	      CAPITAL letters
+lower()	      small letters
+replace()	    Change text
+find()	      Find position
++	            Join strings
+----------------------------------
+
