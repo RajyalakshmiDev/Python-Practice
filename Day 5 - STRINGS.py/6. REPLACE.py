2b37ea665b47a454bcb960a11ee31e264b4de564
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-31 23:10:53 +0530

    6. REPLACE.py
    replace() = Change text

---
+++ Day 5 - STRINGS.py/6. REPLACE.py
@@ -0,0 +1,8 @@
+--- Def: replace() replaces with one value into another value.
+
+Program: convert old name to new name
+text = "I like Java"
+print(text.replace("Java", "Python"))
+
+--Output:
+I like Python
