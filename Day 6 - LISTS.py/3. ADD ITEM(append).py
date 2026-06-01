f11539d3ea957d967bc5fa2251b37ee486d91050
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-06-01 16:01:42 +0530

    3. ADD ITEM(append).py
    Append = Add new item

---
+++ Day 6 - LISTS.py/3. ADD ITEM(append).py
@@ -0,0 +1,9 @@
+--- Def" append() adds a new item to the end of the list.
+
+Program: Add new iteam to the list
+fruits =  ["Mango", "Apple", " Banana", "Guvva"]
+fruits.append("Grapes")
+print(fruits)
+
+---Output:
+['Mango', 'Apple', ' Banana', 'Guvva', 'Grapes']
