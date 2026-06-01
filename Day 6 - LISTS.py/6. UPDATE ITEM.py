10d914e872ab374cffd55fccb7524151c7ae8a60
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-06-02 04:06:20 +0530

    6. UPDATE ITEM.py
    Update items using index

---
+++ Day 6 - LISTS.py/6. UPDATE ITEM.py
@@ -0,0 +1,9 @@
+--- Def:An Existing item can be changed using its index.
+
+-- Program: Replace with item in list
+fruits =  ["Apple", "Banana", "Guvva"]
+fruits [1] = "Orange"
+print(fruits)
+
+--Output:
+['Apple', 'Orange', 'Guvva']
