2396af2874badaa2890885afb45391a507638f4c
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-06-02 03:40:53 +0530

    4.REMOVE ITEM.py
    Remove = Delete item

---
+++ Day 6 - LISTS.py/4.REMOVE ITEM.py
@@ -0,0 +1,9 @@
+--- Def: remove() removes a specific item from the list.
+
+Program: remove item in the list
+fruits =  ["Apple", "Banana", "Guvva"]
+fruits.remove("Banana")
+print(fruits)
+
+--Output:
+['Apple', 'Guvva']
