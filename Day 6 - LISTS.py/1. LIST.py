24e7d4b2767ecf43ef692f578f3e3bebedf2974b
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-06-01 15:28:03 +0530

    1. LIST.py
    List = Many values in one variable

---
+++ Day 6 - LISTS.py/1. LIST.py
@@ -0,0 +1,8 @@
+--- Def: A list is a collection of multiple items stored in a single variable.
+
+Program: Display fruits list
+fruits =  ["Mango", "Apple", " Banana", "Guvva"]
+print(fruits)
+
+--Output:
+['Mango', 'Apple', ' Banana', 'Guvva']
