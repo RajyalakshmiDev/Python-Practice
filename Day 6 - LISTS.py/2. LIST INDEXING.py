94a552d1dc37c7f1f03b6408a11ccec5734e5283
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-06-01 15:51:54 +0530

    2. LIST INDEXING.py
    First Index  = 0

---
+++ Day 6 - LISTS.py/2. LIST INDEXING.py
@@ -0,0 +1,12 @@
+--- Def: Indexing is used to access a specific item from a list.
+
+-> Index always starts with zero(0)
+
+Program: Print indexing of 2 fruits from a list
+fruits =  ["Mango", "Apple", " Banana", "Guvva"]
+print(fruits [0])
+print(fruits [2])
+
+---Output:
+Manago
+Banana
