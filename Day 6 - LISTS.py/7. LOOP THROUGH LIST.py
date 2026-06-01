62bf887f1db168f03f0a8b8b75c818c955d5d8d4
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-06-02 04:17:58 +0530

    7. LOOP THROUGH LIST.py
    Apple
    Banana
    Guvva

---
+++ Day 6 - LISTS.py/7. LOOP THROUGH LIST.py
@@ -0,0 +1,11 @@
+--- Def: A loop can be used to access each item in a list.
+
+Program: 
+fruits =  ["Apple", "Banana", "Guvva"]
+for fruit in fruits:
+    print(fruit)
+
+--- Output:
+Apple
+Banana
+Guvva
