3f455a9dab9e96673a64854af8a22b899efe67b5
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-06-03 08:44:18 +0530

    2. SET.py
    SET = unique values only

---
+++ Day 7 - TUPLE, SET, DICTIONARY.py/2. SET.py
@@ -0,0 +1,25 @@
+--- Def: A set is an unordered collection of unique items.
+
+  ---Creating a set
+  numbers = {10, 20, 30}
+  print(numbers)
+
+---Output:
+  {10, 20, 30}
+
+
+---Duplicate Example
+numbers = {10, 20, 20, 30, 30}
+print(numbers)
+
+---Output:
+{10, 20, 30}
+
+
+----Add Item
+numbers = {10, 20}
+numbers.add(30)
+print(numbers)
+
+---Output:
+ {10, 20, 30}  
